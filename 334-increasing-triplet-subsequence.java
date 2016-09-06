6b157910a1191ea31811ef4069df22c758f4e55d
Kenji Chao <Kenji_Chao@moskastudio.tw>
2016-09-05 17:17:26 -0700

    Solution for 334

---
+++ 334-increasing-triplet-subsequence.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) return false;
+        int small = Integer.MAX_VALUE;
+        int big = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= small) {
+                small = nums[i];
+            } else if (nums[i] <= big) {
+                big = nums[i];
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
