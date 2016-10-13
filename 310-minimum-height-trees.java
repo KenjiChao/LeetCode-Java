ecb95535711548a76b37cffb6339debd1a1367c5
KenjiChao <kenji.chao@sv.cmu.edu>
2016-10-13 01:22:54 -0700

    Revisit 310. Minimum Height Trees

--- 310-minimum-height-trees.java
+++ 310-minimum-height-trees.java
@@ -1,5 +1,35 @@
 public class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Set<Integer>> graph = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new HashSet<Integer>());
+            set.add(i);
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (graph.get(i).size() <= 1) q.add(i);
+        }
+        while (set.size() > 2) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int node = q.poll();
+                set.remove(node);
+                int neighbor = graph.get(node).iterator().next();
+                graph.get(neighbor).remove(node);
+                if (graph.get(neighbor).size() == 1) q.add(neighbor);
+            }
+        }
+        return new ArrayList<Integer>(set);
+    }
+}
+
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         List<Set<Integer>> adj = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
             adj.add(new HashSet<Integer>());
