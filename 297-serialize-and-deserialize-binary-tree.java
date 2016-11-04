742800b81d7a3787fadc103678dd8c31f8912cf5
KenjiChao <kenji.chao@sv.cmu.edu>
2016-11-03 23:26:22 -0700

    Update 297. Serialize and Deserialize Binary Tree

--- 297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree.java
@@ -48,3 +48,52 @@ public class Codec {
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.deserialize(codec.serialize(root));
+
+// BFS
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (sb.length() != 0) sb.append(",");
+            sb.append(node == null ? "X" : node.val);
+            if (node != null) {
+                q.add(node.left);
+                q.add(node.right);
+            }
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] nodes = data.split(",");
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root = null;
+        for (int i = 0; i < nodes.length; i++) {
+            if (i == 0) {
+                root = nodes[i].equals("X") ? null : new TreeNode(Integer.parseInt(nodes[i]));
+                if (root != null) q.add(root);
+            } else {
+                TreeNode node = q.poll();
+                TreeNode left = nodes[i].equals("X") ? null : new TreeNode(Integer.parseInt(nodes[i]));
+                if (left != null) {
+                    q.add(left);
+                    node.left = left;
+                }
+                if (++i < nodes.length) {
+                    TreeNode right = nodes[i].equals("X") ? null : new TreeNode(Integer.parseInt(nodes[i]));
+                    if (right != null) {
+                        q.add(right);
+                        node.right = right;
+                    }
+                }
+            }
+        }
+        return root;
+    }
+}
