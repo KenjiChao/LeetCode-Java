315da6a8946d07e1b7b64236e3e61164ff4f24e2
KenjiChao <kenji.chao@sv.cmu.edu>
2016-11-07 13:42:22 -0800

    Update: 378. Kth Smallest Element in a Sorted Matrix

--- 378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,3 +1,4 @@
+// klogk
 public class Solution {
     class Tuple implements Comparable<Tuple> {
         int val;
@@ -27,5 +28,39 @@ public class Solution {
             pq.add(new Tuple(matrix[tuple.row][tuple.col + 1], tuple.row, tuple.col + 1));
         }
         return pq.peek().val;
+    }
+}
+
+// nlog(max - min)
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        if (n == 0) return -1;
+        int lo = matrix[0][0];
+        int hi = matrix[n - 1][n - 1];
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = getLessOrEqual(matrix, mid);
+            if (count < k) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lo;
+    }
+
+    private int getLessOrEqual(int[][] matrix, int mid) {
+        int n = matrix.length;
+        int i = n - 1, j = 0;
+        int count = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] > mid) i--;
+            else {
+                count += i + 1;
+                j++;
+            }
+        }
+        return count;
     }
 }
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Best Java Solutions for [LeetCode OJ](https://leetcode.com/problemset/algorithms
 - [399. Evaluate Division](https://discuss.leetcode.com/topic/58482/9-lines-floyd-warshall-in-python)
 - [391. Perfect Rectangle](https://discuss.leetcode.com/topic/56052/really-easy-understanding-solution-o-n-java)
 - [388. Longest Absolute File Path](https://discuss.leetcode.com/topic/55247/9-lines-4ms-java-solution)
+- [378. Kth Smallest Element in a Sorted Matrix](https://discuss.leetcode.com/topic/53041/java-1ms-nlog-max-min-solution)
 - [378. Kth Smallest Element in a Sorted Matrix](https://discuss.leetcode.com/topic/52948/share-my-thoughts-and-clean-java-code)
 - [375. Guess Number Higher or Lower II](https://discuss.leetcode.com/topic/51358/java-dp-solution)
 - [375. Guess Number Higher or Lower II - 2](https://discuss.leetcode.com/topic/51487/an-o-n-2-dp-solution-quite-hard)
