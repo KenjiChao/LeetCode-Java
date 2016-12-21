abc37a9bc24bf74d334488a851f97380a321c5d4
KenjiChao <kenji.chao@sv.cmu.edu>
2016-12-21 15:16:54 +0800

    116. Populating Next Right Pointers in Each Node

--- 116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node.java
@@ -18,3 +18,20 @@ public class Solution {
         connect(root.left);
     }
 }
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        TreeLinkNode level = root;
+        TreeLinkNode cur = null;
+        while (level.left != null) {
+            cur = level;
+            while (cur != null) {
+                cur.left.next = cur.right;
+                if (cur.next != null) cur.right.next = cur.next.left;
+                cur = cur.next;
+            }
+            level = level.left;
+        }
+    }
+}
