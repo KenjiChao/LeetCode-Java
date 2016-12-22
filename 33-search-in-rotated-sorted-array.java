b20e907cd62a29894ebbfb6869d16a99d111a67f
KenjiChao <kenji.chao@sv.cmu.edu>
2016-12-22 23:21:36 +0800

    33. Search in Rotated Sorted Array

--- 33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array.java
@@ -23,3 +23,25 @@ public class Solution {
         return -1;
     }
 }
+
+// use only one binary search
+public class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[hi] < nums[mid]) {
+                // left half is sorted
+                if (nums[lo] <= target && target < nums[mid]) hi = mid - 1;
+                else lo = mid + 1;
+            } else {
+                // right half is sorted
+                if (nums[mid] < target && target <= nums[hi]) lo = mid + 1;
+                else hi = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
